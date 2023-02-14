499578ccc70b77061e1b9e328a43d411415e6e76
IngDavidTM <david-1011@hotmail.com>
2023-02-14 11:55:29 -0500

    Add categories index and new

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,27 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.all
+  end
+
+  def show; end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user = current_user
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :home, only: [:index]
-  resources :categories, only: [:index]
+  resources :categories, only: [:index, :show, :new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
