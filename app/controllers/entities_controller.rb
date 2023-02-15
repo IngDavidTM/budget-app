0ebb7e07158be44ebb20439c45e8de3ff09a581c
IngDavidTM <david-1011@hotmail.com>
2023-02-15 12:23:21 -0500

    Add validations

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -10,21 +10,26 @@ class EntitiesController < ApplicationController
   end
 
   def create
-    @entity = Entity.new(entity_params)
-    @entity.user = current_user
-    if @entity.save
-      params[:category_entities][:category_id].each do |category_id|
-        CategoryEntity.create(entity_id: @entity.id, category_id: category_id)
-      end
-      redirect_to category_entities_path(params[:category_id])
+    if params[:category_entities][:category_id].length == 1
+      flash[:alert] = 'You must select at least one category'
+      redirect_to new_category_entity_path(params[:category_id])
     else
-      render :new
+      @entity = Entity.new(entity_params)
+      @entity.user = current_user
+      if @entity.save
+        params[:category_entities][:category_id].each do |category_id|
+          CategoryEntity.create(entity_id: @entity.id, category_id:)
+        end
+        redirect_to category_entities_path(params[:category_id])
+      else
+        render :new
+      end
     end
   end
 
   private
 
   def entity_params
-    params.require(:entity).permit(:name, :amount, category_entities: [:category_id, :entity_id])
+    params.require(:entity).permit(:name, :amount, category_entities: %i[category_id entity_id])
   end
 end
--- app/models/category_entity.rb
+++ app/models/category_entity.rb
@@ -1,5 +1,8 @@
 class CategoryEntity < ApplicationRecord
+  validates :entity_id, presence: true
+  validates :category_id, presence: true
   belongs_to :category
   belongs_to :entity
-  validates :entity, uniqueness: { scope: :category, message: "You can't add the same transaction for the same category"}
+  validates :entity,
+            uniqueness: { scope: :category, message: "You can't add the same transaction for the same category" }
 end
